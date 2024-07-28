9ecf522f54d91f3389af7fb4b5ac08d36bb57052
Thisara-Chamika <thisarachamika2003@gmail.com>
2024-07-28 12:11:36 +0530

    js operations

---
+++ app.js
@@ -0,0 +1,26 @@
+let sum = 10+20;
+
+// + | - | * | / | %
+
+console.log(sum);
+
+// Assignment Operations
+
+// = | += | -+ | *= | /=
+
+//comparison Operations
+
+// == | != | >= | <= | > | <
+
+// ===
+
+let isEqual = (5 == '5');
+
+let isThisEqual = (5 === '5');
+
+console.log(isEqual);
+console.log(isThisEqual);
+
+//Logical Operations
+
+// && | || | !
