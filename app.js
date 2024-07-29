cebd7f6e0fa4d543aab1e8a455c60340a3e95a51
Thisara-Chamika <thisarachamika2003@gmail.com>
2024-07-30 00:36:39 +0530

    events

--- app.js
+++ app.js
@@ -122,26 +122,76 @@
 
 
 
-class customer{
+// class customer{
 
-    constructor(name,age,address){
-        this.name=name;
-        this.age=age;
-        this.address=address;
+//     constructor(name,age,address){
+//         this.name=name;
+//         this.age=age;
+//         this.address=address;
     
-    }
-    setname(name){
-        this.name=name;
-    }
-    getname(name){
-        return this.name;
-    }
-}
+//     }
+//     setname(name){
+//         this.name=name;
+//     }
+//     getname(name){
+//         return this.name;
+//     }
+// }
 
-let Customer = new customer("saman",21,"Panadura");
+// let Customer = new customer("saman",21,"Panadura");
 
-let Customer2 = new customer("kaman",20,"Panadura-South");
+// let Customer2 = new customer("kaman",20,"Panadura-South");
 
-console.log(Customer.getname());
-Customer2.setname("Somapala");
-console.log(Customer2);
+// console.log(Customer.getname());
+// Customer2.setname("Somapala");
+// console.log(Customer2);
+
+
+
+// let customers = [
+//         {
+//             name:"saman",
+//             age:"12"
+//         },
+//         {
+//             name:"kamal",
+//             age:"20"
+//         },
+//         {
+//             name:"vimal",
+//             age:"32"
+//         },
+//         {
+//             name:"nimal",
+//             age:"15"
+//         },
+//         {
+//             name:"sunil",
+//             age:"50",
+//             address:"gampaha"
+//         },
+//         {
+//             name:"ranil",
+//             age:"30"
+//         }
+//     ]
+    
+//     for(let i = 0; i<customers.length; i++){
+//         console.log(customers[i].name +" | "+ customers[i].age);
+//         console.log();
+//     }
+
+
+
+let numbers = [5,8,9,12,1,7,8,2,3,2]
+
+let words = ["bggdf","fdsf","asdf","rrgb","csdf","dfdg","zxasd","vsd","po","re"]
+
+numbers.push(100);
+numbers.push(200);
+
+numbers.pop();
+
+console.log(numbers.sort());
+
+console.log(words.sort());
